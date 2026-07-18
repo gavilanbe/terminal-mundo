6d4fe0db5e5e3a6e644523e5f062e79038affdb9
gavilanbe <112889199+gavilanbe@users.noreply.github.com>
2026-07-18 06:39:26 +0200

    Terminal Mundo: PWA para aprender países, capitales y banderas

---
+++ sw.js
@@ -0,0 +1,37 @@
+const V = "tm-v1";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(V).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== V).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET" || !e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit =>
+      hit ||
+      fetch(e.request).then(res => {
+        if (res.ok) {
+          const copy = res.clone();
+          caches.open(V).then(c => c.put(e.request, copy));
+        }
+        return res;
+      })
+    )
+  );
+});
